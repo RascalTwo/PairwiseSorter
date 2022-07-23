9cb6056e22e1e649f394d09a3bb94a9afa2344d8
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-23 19:02:00 -0400

    Add tabs to list contents

---
+++ public/list.js
@@ -0,0 +1,28 @@
+
+const links = [...document.querySelectorAll('a[href^="#"]')];
+
+const validTabs = links.map(link => link.getAttribute('href').slice(1));
+if (validTabs.indexOf(window.location.hash) === -1) window.location.hash = validTabs[1];
+
+const markActive = link => {
+	const oldActive = links.find(link => link.classList.contains('active'));
+	if (oldActive) {
+		oldActive.classList.remove('active');
+		oldActive.ariaCurrent = 'false';
+	}
+
+	link.classList.add('active');
+	link.ariaCurrent = 'page';
+};
+
+markActive(links.find(link => link.getAttribute('href') === window.location.hash));
+
+links.forEach(link => link.addEventListener('click', event => {
+	event.preventDefault();
+
+	history.pushState({}, '', link.href);
+	history.pushState({}, '', link.href);
+	history.back();
+
+	markActive(link);
+}));
