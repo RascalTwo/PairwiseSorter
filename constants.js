3ee8deaabdbe52db3a7d845a5015d6b0db19317a
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-03 14:57:37 -0400

    Add morgan logging

--- constants.js
+++ constants.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 const PORT = process.env.PORT || 1337;
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/pairwise-sorter';
 const JWT_SECRET = process.env.JWT_SECRET || 'secret';
+const NODE_ENV = process.env.NODE_ENV;
 
-module.exports = { PORT, MONGODB_URL, JWT_SECRET };
+module.exports = { PORT, MONGODB_URL, JWT_SECRET, NODE_ENV };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const { NODE_ENV } = require('./constants');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'));
 app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
