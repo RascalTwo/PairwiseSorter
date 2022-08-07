a8de883afec42dfc3c5c1e82eb951958d8e66b8f
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-06 20:04:04 -0400

    Fix missing JSX key

--- views/components/ComparisonsList.jsx
+++ views/components/ComparisonsList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({ list, isOwner, denormalizedComparisons }) => <ul className="list-group comparisons-list" id="comparisons-tab">
 	{denormalizedComparisons.map(comparison =>
-		<li className="list-group-item d-flex justify-content-between align-items-center position-relative">
+		<li key={comparison.a + '-' + comparison.b} className="list-group-item d-flex justify-content-between align-items-center position-relative">
 			<div>{list.items.find(item => item._id == comparison.a).name}</div>
 			<ul className="position-absolute top-50 start-50 translate-middle">
 				<div>{comparison.result === 1 ? '<' : comparison.result === 0 ? '===' : '>'}</div>
