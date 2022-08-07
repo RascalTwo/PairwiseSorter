546e728bc9f39c464f70048628f26db089043c1e
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-06 23:04:48 -0400

    Make constants dynamic

--- constants.js
+++ constants.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 1337;
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/pairwise-sorter';
-const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
-const NODE_ENV = process.env.NODE_ENV;
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-
-
-module.exports = { PORT, MONGODB_URL, SESSION_SECRET, NODE_ENV, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET };
+module.exports = [
+	['PORT', 1337],
+	['MONGODB_URL', 'mongodb://localhost:27017/pairwise-sorter'],
+	['SESSION_SECRET', 'secret'],
+	['NODE_ENV'],
+	['GOOGLE_CLIENT_ID'],
+	['GOOGLE_CLIENT_SECRET'],
+	['DISCORD_CLIENT_ID'],
+	['DISCORD_CLIENT_SECRET'],
+	['GITHUB_CLIENT_ID'],
+	['GITHUB_CLIENT_SECRET'],
+].reduce((acc, [key, defaultValue]) => {
+	acc[key] = process.env[key] || defaultValue;
+	return acc;
+}, {});
